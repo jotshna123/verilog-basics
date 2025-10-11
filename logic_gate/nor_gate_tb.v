5904b48f9d4a6b0e2d3927c2532359d960ea9087
Jotshna <jotshnatulasi@gmail.com>
2025-10-11 15:31:01 +0530

    Create nor_gate_tb.v

---
+++ logic_gate/nor_gate_tb.v
@@ -0,0 +1,15 @@
+module nor_gate_tb;
+reg a,b;
+wire y;
+nor_gate dut(.a(a),.b(b),.y(y));
+initial
+begin
+$dumpfile("wave_nor.vcd");
+$dumpvars(0,nor_gate_tb);
+a=0;b=0; #10 $display("a=%b,b=%b,y=%b",a,b,y);
+a=0;b=1; #10 $display("a=%b,b=%b,y=%b",a,b,y);
+a=1;b=0; #10 $display("a=%b,b=%b,y=%b",a,b,y); 
+a=1;b=1; #10 $display("a=%b,b=%b,y=%b",a,b,y);
+#10 $finish;
+end 
+endmodule
